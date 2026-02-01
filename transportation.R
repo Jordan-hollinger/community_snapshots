4fa6b0907218275930be162748a070968b2a97f2
Jordan-hollinger <jordan.a.hollinger@gmail.com>
2026-02-01 15:24:56 -0500

    Updated with with one global function for running all topics. Just one instance of running the pull.

--- transportation.R
+++ transportation.R
@@ -3,29 +3,74 @@ library(tidyverse)
 library(writexl)
 
 ####set personal access token (current PAT expires Sun, Mar 29 2026) and Census API Key####
-
 #To set github PAT:
 #gitcreds::gitcreds_set()
 
 #if you do not have a census api key set globally in R, you will need to do that via: 
 # census_api_key("YOUR KEY GOES HERE", install = TRUE)
 
+#best to use detailecd tables for MA Towns. Can also use Data Profiles, but beware that variable ids may not be consistent between years.
 
-####Initial setup - var list, global vars and functions####
-
-communities <- c("Auburn", "Barre", "Berlin", "Blackstone", "Boylston", "Brookfield", "Charlton", "Douglas", "Dudley", "East Brookfield", "Grafton", "Hardwick", "Holden", "Hopedale", "Leicester", "Mendon", "Millbury", "Millville", "New Braintree", "North Brookfield", "Northborough", "Northbridge", "Oakham", "Oxford", "Paxton", "Princeton", "Rutland", "Shrewsbury", "Southbridge", "Spencer", "Sturbridge", "Sutton", "Upton", "Uxbridge", "Warren", "Webster", "West Boylston", "West Brookfield", "Westborough", "Worcester")
-
-##best to use detailecd tables for MA Towns. Can also use Data Profiles, but beware that variable ids may not be consistent between years.
-
-#load variables for a particular year
+####load and find the vars you want####
 vars_2023 <- load_variables(2023, "acs5", cache = TRUE)
 
-#years for variable pulls
+# --- Exploration helpers (run when adding new topics) ---
+vars_B08303 <- vars_2023 |> filter(str_starts(name, "B08303"))
+vars_B08201 <- vars_2023 |> filter(str_starts(name, "B08201"))
+
+
+####Global variables####
+
+#All Towns/Cities
+communities <- c("Auburn", "Barre", "Berlin", "Blackstone", "Boylston", "Brookfield", "Charlton", "Douglas", "Dudley", "East Brookfield", "Grafton", "Hardwick", "Holden", "Hopedale", "Leicester", "Mendon", "Millbury", "Millville", "New Braintree", "North Brookfield", "Northborough", "Northbridge", "Oakham", "Oxford", "Paxton", "Princeton", "Rutland", "Shrewsbury", "Southbridge", "Spencer", "Sturbridge", "Sutton", "Upton", "Uxbridge", "Warren", "Webster", "West Boylston", "West Brookfield", "Westborough", "Worcester")
+
+#Years to pull
 years <- c(2010:2023)
+
+#Topic specs
+topic_specs <- list(
+  list(
+    topic = "commute_mode",
+    table = "B08301",
+    vars  = c("B08301_003", "B08301_004", "B08301_010", "B08301_016",
+              "B08301_017", "B08301_018", "B08301_019", "B08301_020", "B08301_021")
+  ),
+  list(
+    topic = "vehicle_avail",
+    table = "B08201",
+    vars  = c("B08201_002", "B08201_003", "B08201_004", "B08201_005", "B08201_006")
+  ),
+  list(
+    topic = "travel_time",
+    table = "B08303",
+    vars  = c("B08303_002","B08303_003","B08303_004","B08303_005","B08303_006",
+              "B08303_007","B08303_008","B08303_009","B08303_010","B08303_011",
+              "B08303_012","B08303_013")
+  )
+)
 
 #excel and csv file location
 xlsx_path <- "data/transportation/xlsx/"
 csv_path <- "data/transportation/csv/"
+
+####Functions####
+
+#Missing year checker for variables
+check_vars_by_year <- function(spec, years, survey = "acs5") {
+  purrr::map_dfr(years, function(yr) {
+    vars_yr <- load_variables(yr, survey, cache = TRUE)
+    
+    missing <- setdiff(spec$vars, vars_yr$name)
+    
+    tibble(
+      topic = spec$topic,
+      year = yr,
+      n_missing = length(missing),
+      missing_vars = paste(missing, collapse = ", ")
+    )
+  }) |>
+    filter(n_missing > 0)
+}
 
 #global export function
 export_csv_xlsx <- function(df, filename, csv_path, xlsx_path) {
@@ -88,116 +133,49 @@ make_label_lookup <- function(vars_tbl, prefix = "Estimate!!Total:!!") {
     )
 }
 
-
-####Commute Mode to work####
-
-#variables from ACS
-commute_mode_vars_all <- vars_2023 |> 
-  filter(str_detect(name, regex("B08301", ignore_case = TRUE ))) 
-
-#just the ones we want
-commute_mode_vars_select <- commute_mode_vars_all |> 
-  filter(
-    name %in% c("B08301_003", "B08301_004", "B08301_010", "B08301_016", "B08301_017", "B08301_018", "B08301_019", "B08301_020", "B08301_021")
-  ) |> 
-  left_join(make_label_lookup(commute_mode_vars_all), by = "name")
-
-#vector for use in function
-commute_mode_vars_sub <- commute_mode_vars_select |> 
-  pull(name)
-
-#pull data using our global function
-
-commute_mode_all <- pull_acs_multiyear(
-  var_ids = commute_mode_vars_sub,
-  years = years,
-  communities = communities
-)
-
-#join short label from vars
-commute_mode_all <- commute_mode_all |> 
-  left_join(commute_mode_vars_select |> select(label_short, name), join_by(variable == name))
-
-#export to csv and excel
-export_csv_xlsx(
-  df = commute_mode_all,
-  filename = "commute_mode",
-  csv_path = csv_path,
-  xlsx_path = xlsx_path
-)
-
-
-####Vehicles available####
-
-#variables from ACS
-vehicle_avail_vars_all <- vars_2023 |> 
-  filter(str_detect(name, regex("B08201", ignore_case = TRUE ))) 
-
-#just the ones we want
-vehicle_avail_vars_select <- vehicle_avail_vars_all |> 
-  filter(
-    name %in% c("B08201_002", "B08201_003", "B08201_004", "B08201_005", "B08201_006")
-  )|> 
-  left_join(make_label_lookup(vehicle_avail_vars_all), by = "name")
+#run evertying
+run_topic <- function(spec, vars_master, years, communities, csv_path, xlsx_path) {
   
-#just the ones we want in a vector for use in function
-vehicle_avail_vars_sub <- vehicle_avail_vars_select |> 
-  pull(name)
+  # variables from ACS for the table
+  vars_all <- vars_master |>
+    filter(str_starts(name, spec$table))
+  
+  # just the ones we want + short labels
+  vars_select <- vars_all |>
+    filter(name %in% spec$vars) |>
+    left_join(make_label_lookup(vars_all), by = "name")
+  
+  # pull data and attach labels
+  df <- pull_acs_multiyear(
+    var_ids = vars_select$name,
+    years = years,
+    communities = communities
+  ) |>
+    left_join(vars_select |> select(name, label_short),
+              join_by(variable == name))
+  
+  # export
+  export_csv_xlsx(
+    df = df,
+    filename = spec$topic,
+    csv_path = csv_path,
+    xlsx_path = xlsx_path
+  )
+  
+  df
+}
 
-#pull data using our global function
 
-vehicle_avail_all <- pull_acs_multiyear(
-  var_ids = vehicle_avail_vars_sub,
+####Run Data Pull####
+topic_results <- map(
+  topic_specs,
+  run_topic,
+  vars_master = vars_2023,
   years = years,
-  communities = communities
-)
-
-#join short label from vars
-vehicle_avail_all <- vehicle_avail_all |> 
-  left_join(vehicle_avail_vars_select |> select(label_short, name), join_by(variable == name))
-
-#export to csv and excel
-export_csv_xlsx(
-  df = vehicle_avail_all,
-  filename = "vehicle_avail",
+  communities = communities,
   csv_path = csv_path,
   xlsx_path = xlsx_path
 )
 
+names(topic_results) <- map_chr(topic_specs, "topic")
 
-####Travel time to work####
-
-#variables from ACS
-travel_time_vars_all <- vars_2023 |> 
-  filter(str_detect(name, regex("B08303", ignore_case = TRUE ))) 
-
-#just the ones we want
-travel_time_vars_select <- travel_time_vars_all |> 
-  filter(
-    name %in% c("B08303_002","B08303_003","B08303_004","B08303_005", "B08303_006", "B08303_007", "B08303_008", "B08303_009", "B08303_010", "B08303_011", "B08303_012", "B08303_013")
-  )|> 
-  left_join(make_label_lookup(travel_time_vars_all), by = "name")
-
-#just the ones we want in a vector for use in function
-travel_time_vars_sub <- travel_time_vars_select |> 
-  pull(name)
-
-#pull data using our global function
-
-travel_time_all <- pull_acs_multiyear(
-  var_ids = travel_time_vars_sub,
-  years = years,
-  communities = communities
-)
-
-#join short label from vars
-travel_time_all <- travel_time_all |> 
-  left_join(travel_time_vars_select |> select(label_short, name), join_by(variable == name))
-
-#export to csv and excel
-export_csv_xlsx(
-  df = travel_time_all,
-  filename = "travel_time",
-  csv_path = csv_path,
-  xlsx_path = xlsx_path
-)
